dfa423a5f98c4ecbc3c0cbeb130dba0e4b22ccf4
Metin Karakaya <145351839+KarakayaMetin8787@users.noreply.github.com>
2024-03-12 15:44:31 +0100

    created doJwtAuth.js and deleted makeJwtAuth.js

---
+++ backend/src/middleware/doJwtAuth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+export async function doJwtAuth(req, res, next) {
+    const _invalidAuthResponse = (message) =>
+    res
+        .status(401)
+        .json({ success: false, message: message || "Invalid authentication" });
+    try {
+        const tokenString = extractTokenFromRequest();
+        const tokenPayload = jwt.verify(tokenString, process.env.JWT_SECRET);
+        req.verifiedUserClaims = tokenPayload;
+        next();
+    } catch (error) {
+        console.log(error);
+        return _invalidAuthResponse("Invalid token");
+    }
+    function extractTokenFromRequest() {
+        const authorization = req.headers.authorization;
+        if (authorization) {
+            const [authType, tokenString] = authorization.split(" ");
+            if (authType !== "Bearer" || !tokenString) return null;
+            else return tokenString;
+        } else {
+            return req.session.refreshToken;
+        }
+    }
+}
