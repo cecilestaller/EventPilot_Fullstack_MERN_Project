a8dd8037ece3bd9a2403364a2a1eafb751ffe6aa
Metin Karakaya <145351839+KarakayaMetin8787@users.noreply.github.com>
2024-03-12 14:52:53 +0100

    started page Home implementation

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,41 @@
 import Nav from "../../components/nav/Nav";
 import "./Home.scss";
+import Logo from "../../assets/images/Logo.svg";
+import DropdownArrow from "../../assets/images/location_dropdown_arrow.svg"
+import NearbyEvents from "../../components/nearbyEvents/NearbyEvents";
+import UpcomingEvents from "../../components/upcomingEvents/UpcomingEvents";
+import RandomEvent from "../../components/randomEvent/RandomEvent";
+import SeeAllArrow from "../../assets/images/seeall_arrow.svg"
 
 const Home = () => {
     return (
-        <>
-            <h2>Hi ich bin Home</h2>
+        <div className="homeContainer">
+            <header className="headerHome">
+                <img className="logo" src={Logo} alt="logoIcon" />
+                <div className="locationContainer">
+                    <div className="locationDropdownContainer">
+                        <p className="locationTitle">Standort</p>
+                        <img src={DropdownArrow} alt="arrowIcon" />
+                    </div>
+                    <h3 className="showCurrentLocation">Berlin, DE</h3>
+                </div>
+                <div className="emptyDivForFlexSpace">
+                    <img className="logo" style={{visibility: "hidden"}} src={Logo} alt="logoIcon" />
+                </div>
+            </header>
+            <div className="UpcomingTitleContainer">
+                <p>Anstehende Events</p>
+                <label >Alle zeigen<img src={SeeAllArrow} alt="seeAllIcon" /></label>
+            </div>
+            <UpcomingEvents/>
+            <div className="NearbyTitleContainer">
+                <p>In deiner Nähe</p>
+                <label >Alle zeigen<img src={SeeAllArrow} alt="seeAllIcon" /></label>
+            </div>
+            <NearbyEvents/>
+            <RandomEvent/>
             <Nav/>
-        </>
+        </div>
     );
 };
 
