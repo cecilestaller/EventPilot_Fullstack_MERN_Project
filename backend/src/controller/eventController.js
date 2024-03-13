5169adf91b3e68373b2783bb9fcaf654600bdbb0
cecilestaller <c.staller96@gmail.com>
2024-03-13 13:50:01 +0100

    BE: add fillRegisteredGuests Array of Event endpoint which also checks if event is fullyBooked

--- backend/src/controller/eventController.js
+++ backend/src/controller/eventController.js
@@ -3,38 +3,51 @@ import { EventService } from "../services/index.js";
 
 // ==== Add event ====
 export const postNewEventCtrl = catchAsync(
-  async (req, res) => {
-    const authenticatedUserId = req.verifiedUserClaims.sub;
-    const eventInfo = req.body;
-    const result = await EventService.addNewEvent(
-      authenticatedUserId,
-      eventInfo
-    );
-    res.status(201).json({ success: true, result });
-  },
-  { message: "Could not create new Event" }
+    async (req, res) => {
+        const authenticatedUserId = req.verifiedUserClaims.sub;
+        const eventInfo = req.body;
+        const result = await EventService.addNewEvent(
+            authenticatedUserId,
+            eventInfo
+        );
+        res.status(201).json({ success: true, result });
+    },
+    { message: "Could not create new Event" }
 );
 
 // ==== View all events ====
 export const getAllEventsCtrl = catchAsync(
-  async (req, res) => {
-    const authenticatedUserId = req.verifiedUserClaims.sub;
-    const result = await EventService.getAllEvents(authenticatedUserId);
-    res.status(200).json({ success: true, result });
-  },
-  { message: "Coult not retrieve events" }
+    async (req, res) => {
+        const authenticatedUserId = req.verifiedUserClaims.sub;
+        const result = await EventService.getAllEvents(authenticatedUserId);
+        res.status(200).json({ success: true, result });
+    },
+    { message: "Coult not retrieve events" }
 );
 
 // ==== Get event details ====
 export const getEventDetailsCtrl = catchAsync(
-  async (req, res) => {
-    const authenticatedUserId = req.verifiedUserClaims.sub;
-    const eventId = req.body;
-    const result = await EventService.getEventDetails(
-      authenticatedUserId,
-      eventId
-    );
-    res.status(200).json({ success: true, result });
-  },
-  { message: "Could not retrieve event details" }
+    async (req, res) => {
+        const authenticatedUserId = req.verifiedUserClaims.sub;
+        const eventId = req.params.eventId;
+        const result = await EventService.getEventDetails(
+            authenticatedUserId,
+            eventId
+        );
+        res.status(200).json({ success: true, result });
+    },
+    { message: "Could not retrieve event details" }
+);
+
+export const patchFillRegisteredGuestCheckFullyBookedCtrl = catchAsync(
+    async (req, res) => {
+        const authenticatedUserId = req.verifiedUserClaims.sub;
+        const eventId = req.params.eventId;
+        const result = await EventService.fillRegisteredGuestsArray(
+            authenticatedUserId,
+            eventId
+        );
+        res.status(200).json({ success: true, result });
+    },
+    { message: "Could not retrieve event details" }
 );
