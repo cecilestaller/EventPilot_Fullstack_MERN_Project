bd35477480880e2d9dcb0573f914fb454db4898b
Metin Karakaya <145351839+KarakayaMetin8787@users.noreply.github.com>
2024-03-13 10:35:35 +0100

    created not finished navbar, added variable colors in app.scss

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,31 +6,50 @@ import NearbyEvents from "../../components/nearbyEvents/NearbyEvents";
 import UpcomingEvents from "../../components/upcomingEvents/UpcomingEvents";
 import RandomEvent from "../../components/randomEvent/RandomEvent";
 import SeeAllArrow from "../../assets/images/seeall_arrow.svg"
+import { useNavigate } from "react-router-dom"
 
 const Home = () => {
+
+    const navigate = useNavigate()
+
+    // ========= function of "Alle zeigen" in "Anstehende Events" ===================
+    const forwardToSeeAllUpcoming = () => {
+        navigate("/search")
+    }
+    // ========= function of "Alle zeigen" in "In der Nähe" ===================
+    const forwardToSeeAllNearby = () => {
+        navigate("/search")
+    }
+    
     return (
         <div className="homeContainer">
             <header className="headerHome">
                 <img className="logo" src={Logo} alt="logoIcon" />
                 <div className="locationContainer">
-                    <div className="locationDropdownContainer">
+                    <div className="locationDropdownContainer" onClick={() => locationDropdownMenu()}>
                         <p className="locationTitle">Standort</p>
                         <img src={DropdownArrow} alt="arrowIcon" />
                     </div>
                     <h3 className="showCurrentLocation">Berlin, DE</h3>
                 </div>
+                {/* Element emptyDivForFlexSpace is invisible and only used to properly center  locationDropdownContainer */}
                 <div className="emptyDivForFlexSpace">
                     <img className="logo" style={{visibility: "hidden"}} src={Logo} alt="logoIcon" />
                 </div>
+                {/* ===================================================================================================== */}
             </header>
             <div className="UpcomingTitleContainer">
                 <p className="titleOfConponent">Anstehende Events</p>
-                <label className="seeAllTextAndIcon" >Alle zeigen<img src={SeeAllArrow} alt="seeAllIcon" /></label>
+                <label className="seeAllTextAndIcon" onClick={() => forwardToSeeAllUpcoming()}>
+                    Alle zeigen <img src={SeeAllArrow} alt="seeAllIcon" />
+                </label>
             </div>
             <UpcomingEvents/>
             <div className="NearbyTitleContainer">
                 <p className="titleOfConponent">In deiner Nähe</p>
-                <label className="seeAllTextAndIcon" >Alle zeigen<img src={SeeAllArrow} alt="seeAllIcon" /></label>
+                <label className="seeAllTextAndIcon" onClick={() => forwardToSeeAllNearby()}>
+                    Alle zeigen <img src={SeeAllArrow} alt="seeAllIcon" />
+                </label>
             </div>
             <NearbyEvents/>
             <RandomEvent/>
