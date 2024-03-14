d73652f1b20bcaa406b86099b7b8884775c5ffff
MoniqueHeusinger <m.heusinger1821@gmail.com>
2024-03-14 15:56:16 +0100

    FE: start adding favicon - not working yet

--- backend/src/services/signUpUser.js
+++ backend/src/services/signUpUser.js
@@ -1,23 +1,23 @@
 import { User } from "../models/index.js";
 
 export async function signUpUser({ userName, email, password }) {
-    // check if user with same email or userName already exists
-    const foundUserWithSameEmail = await User.find({ email: email });
-    if (foundUserWithSameEmail.length > 0)
-        throw new Error("User with this email already exists");
-    const foundUserWithSameUserName = await User.find({ userName: userName });
-    if (foundUserWithSameUserName.length > 0)
-        throw new Error("User with user name already exists");
+  // check if user with same email or userName already exists
+  const foundUserWithSameEmail = await User.find({ email: email });
+  if (foundUserWithSameEmail.length > 0)
+    throw new Error("User with this email already exists");
+  const foundUserWithSameUserName = await User.find({ userName: userName });
+  if (foundUserWithSameUserName.length > 0)
+    throw new Error("User with user name already exists");
 
-    const user = await User.create({
-        userName,
-        email,
-        password,
-    });
+  const user = await User.create({
+    userName,
+    email,
+    password,
+  });
 
-    return userToProfileInfo(user);
+  return userToProfileInfo(user);
 }
 
 function userToProfileInfo({ _id, userName, email }) {
-    return { _id, userName, email };
+  return { _id, userName, email };
 }
