da1e98da02ada72b705c48e296373b8ee929af5d
KWiefel <dev.kwiefel427@gmail.com>
2024-03-15 16:37:02 +0100

    added splashscreen.jsx

--- frontend/src/pages/splashScreen/Splash.jsx
+++ frontend/src/pages/splashScreen/Splash.jsx
@@ -1,19 +1,27 @@
-import { useNavigate } from "react-router-dom";
 import "./Splash.scss";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import logo from "../../assets/images/Logo.svg";
 
 const Splash = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const forwardToLogin = () => {
+    navigate("/login");
+  };
 
-    const forwardToLogin = () => {
-        navigate("/login")
-    }
-
-    return (
-        <div onClick={() => forwardToLogin()}>
-            <h2>Hi ich bin Splash</h2>
+  return (
+    <div onClick={() => forwardToLogin()}>
+      <div className="splash__wrapper">
+        <div className="splash_logo">
+          <img src={logo} alt="" />
+          <p className="splash_logo-title">
+            vent<span className="splash_logo-title-span">Pilot</span>{" "}
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Splash;
